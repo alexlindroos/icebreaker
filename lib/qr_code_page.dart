c121196e5d2fc0f3620ef29f8b2f4229a2a0104f
Iiro Krankka <iiro.krankka@gmail.com>
2019-06-01 16:00:13 +0300

    Matchmaking support

--- lib/qr_code_page.dart
+++ lib/qr_code_page.dart
@@ -1,42 +1,99 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:flutter/services.dart';
-
+import 'package:icebreaker/find_a_match_task.dart';
+import 'package:icebreaker/routes.dart';
+import 'package:icebreaker/user_match.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class QrCodePage extends StatefulWidget {
   @override
-  _QrCodePageState createState() {
-    return _QrCodePageState();
-  }
+  _QrCodePageState createState() => _QrCodePageState();
 }
 
 class _QrCodePageState extends State {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  UserMatch _match;
+
+  @override
+  void initState() {
+    super.initState();
+    _startListeningToMatchEvents();
+    _findAMatch();
+  }
+
+  Future<void> _startListeningToMatchEvents() async {
+    final myUserId = (await FirebaseAuth.instance.signInAnonymously()).uid;
+
+    StreamSubscription<DocumentSnapshot> subscription;
+    subscription = Firestore.instance
+        .collection('events')
+        .document('hack19-helsinki')
+        .collection('match-making-pool')
+        .document(myUserId)
+        .snapshots()
+        .listen((data) {
+      if (!mounted) return;
+
+      if (data != null && data.exists) {
+        final available = data['available'];
+        final matchedUserId = data['matchedUserId'];
+
+        if (!available && matchedUserId != null) {
+          print('Its a match! Say Hello $matchedUserId');
+          subscription.cancel();
+          Navigator.pushReplacementNamed(context, Routes.thankYou);
+        }
+      }
+    });
+  }
+
+  Future<void> _findAMatch() async {
+    final task = FindAMatchTask(FirebaseAuth.instance, Firestore.instance);
+    final match = await task.run('hack19-helsinki');
+
+    setState(() {
+      _match = match;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          centerTitle: true,
-          title: Text('QR Code Scanner'),
-        ),
-      body: Column(
-        children: <Widget>[
+    Widget _content;
+
+    if (_match == null) {
+      _content = Center(
+        child: Text('Searching for a match for you...'),
+      );
+    } else {
+      _content = Column(
+        children: [
           Expanded(
-            child: Container(child: _qrCode(),),
+            child: Container(
+              child: _qrCode(),
+            ),
           ),
           Expanded(
             child: Container(
-              child: QRView(
-                key: qrKey,
-                onQRViewCreated: _onQRViewCreated,
-              )
-          ),
+                child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            )),
           ),
         ],
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('QR Code Scanner'),
       ),
+      body: _content,
       resizeToAvoidBottomPadding: true,
     );
   }
