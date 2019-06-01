72c1bac62d3504a0e983398e4e42a416313992cf
Alex Lindroos <alex_lindroos@hotmail.com>
2019-06-01 13:44:57 +0300

    EnterCodePage added as a first page.

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ThankYouPage(),
+      home: EnterCodePage(),
       routes: {
         Routes.enterCode: (_) => EnterCodePage(),
         Routes.takePhoto: (_) => TakePhotoPage(),
